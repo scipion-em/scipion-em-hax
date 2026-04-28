183884a9b9de62ab2842fe6d40a1cb1c2f1414db
EduGitt <edugarde01@gmail.com>
2026-04-28 09:07:48 +0200

    tomosiren branch for sharpening method and updates on multiple files (on-going project)

--- hax/protocols/__init__.py
+++ hax/protocols/__init__.py
@@ -37,3 +37,4 @@ from .protocol_volume_gray_scale_adjustment import JaxProtVolumeAdjustment
 from .protocol_reconstruct_modart import JaxProtReconstructMoDART
 from .protocol_zernike3deep import JaxProtTrainZernike3Deep
 from .protocol_filter_latent_space import JaxProtFilterLatents
+from .protocol_sharpening import JaxProtSharpeningHetSiren
